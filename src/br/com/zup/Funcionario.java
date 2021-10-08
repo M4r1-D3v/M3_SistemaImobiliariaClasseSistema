6e78d4dcf635bf9ecb9c98053d4e7e58af37024e
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-08 08:38:06 -0300

    Modelar classe Funcionário

--- src/br/com/zup/Funcionario.java
+++ src/br/com/zup/Funcionario.java
@@ -1,4 +1,25 @@
 package br.com.zup;
 
-public class Funcionario {
+public class Funcionario extends Pessoa{
+    private String ctps;
+
+    public Funcionario(String nome, String cpf, String ctps) {
+        super(nome, cpf);
+        this.ctps = ctps;
+    }
+
+    public String getCtps() {
+        return ctps;
+    }
+
+    public void setCtps(String ctps) {
+        this.ctps = ctps;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("Por favor, digite o número da carteira de trabalho: " + getCtps());
+        return retorno.toString();
+    }
 }
