c20aa27ac3038f69eee1c927916cd7ab8b068fd9
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-08 08:51:36 -0300

    Modelar classe Imobiliária

--- src/br/com/zup/Imobiliaria.java
+++ src/br/com/zup/Imobiliaria.java
@@ -1,4 +1,30 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imobiliaria {
+    List<Imovel>imoveis = new ArrayList<>();
+
+    public Imobiliaria() {
+    }
+
+    public Imobiliaria(List<Imovel> imoveis) {
+        this.imoveis = imoveis;
+    }
+
+    public List<Imovel> getImoveis() {
+        return imoveis;
+    }
+
+    public void setImoveis(List<Imovel> imoveis) {
+        this.imoveis = imoveis;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("Imóveis cadastrados: " + getImoveis());
+        return retorno.toString();
+    }
 }
