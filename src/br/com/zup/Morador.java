555b5f6737e9c81d3880ca82a72522f6157a6a3f
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-08 06:05:06 -0300

    Modelar classe Morador

--- src/br/com/zup/Morador.java
+++ src/br/com/zup/Morador.java
@@ -1,4 +1,25 @@
 package br.com.zup;
 
-public class Morador {
+public class Morador extends Pessoa{
+    private String telefone;
+
+    public Morador(String nome, String cpf, String telefone) {
+        super(nome, cpf);
+        this.telefone = telefone;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("Por favor, digite o telefone do morador: ");
+        return retorno.toString();
+    }
 }
